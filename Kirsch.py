82ba1ec266d7a95a9c2c944e1d2887019d2213f5
Eureka3301 <32921341+Eureka3301@users.noreply.github.com>
2022-12-22 04:39:32 +0300

    Add files via upload

--- Kirsch.py
+++ Kirsch.py
@@ -1,169 +1,174 @@
 import numpy as np
-from numpy.linalg import inv
 
-def triag_square(x, y):
-    a = np.array([
-        [1, x[0], y[0]],
-        [1, x[1], y[1]],
-        [1, x[2], y[2]]
+# plate 5mx5m with 1m round hole in x=0 y=0; x=5m has 1e+3 Pa
+
+file_nodes = 'nodes_test.txt'
+file_elements = 'elements_test.txt'
+u_x = 'u_x.txt'
+u_y = 'u_y.txt'
+
+
+
+file = open(file_nodes, 'r')
+line = file.readline()
+Xnode = []
+Ynode = []
+node_num = 0
+while True:
+    line = file.readline()
+    if not line:
+        break
+    [x, y] = list(map(float, line.split()[1:3]))
+    Xnode.append(x)
+    Ynode.append(y)
+    node_num += 1
+
+
+
+file = open(file_elements, 'r')
+line = file.readline()
+ijk = []
+N = []
+elem_num = 0
+while True:
+    line = file.readline()
+    if not line:
+        break
+    l = []
+    for n in list(map(int, line.split()[2:])):
+        l.append(n-1)
+    A = np.array([
+        [1, Xnode[l[0]], Ynode[l[0]]],
+        [1, Xnode[l[1]], Ynode[l[1]]],
+        [1, Xnode[l[2]], Ynode[l[2]]]
     ])
-    return 0.5*np.linalg.det(a)
+    N.append(np.linalg.inv(A))
+    ijk.append(l)
+    elem_num += 1
+
+# Ne = N[e], Ne[p][q] q - 0 -- i, 1 -- j, 2 -- k; p - coefficient within 0 -- 1, 1 -- x, 2 -- y
+
+
+
+def area(x, y):
+    A = np.array([
+        [x[0], y[0], 1.0],
+        [x[1], y[1], 1.0],
+        [x[2], y[2], 1.0]
+    ])
+    return 0.5*np.linalg.det(A)
 
 def dist(x, y):
-    return np.square((x[1]-x[0])**2+(y[1]-y[0])**2)
+    return np.square((x[0]-x[1])**2 + (y[0]-y[1])**2)
 
-class Element:
-    def __init__(self, nodenums, invA) -> None:
-        self.nd_nums = nodenums
-        invA = np.array(invA)
-        self.N = [invA[:,0], invA[:,1], invA[:,2]]
-
-class Node:
-    def __init__(self, X, Y) -> None:
-        self.X = X
-        self.Y = Y
-
-class Mesh():
-    def __init__(self, nodes_input, elements_input) -> None:
-        self.get_nodes(nodes_input)
-        self.get_elements(elements_input)
-
-    def nodes_in_loc(self, x, y):
-        res = []
-        if x == 'any' and y != 'any':
-            for nd_num in range(1, self.nodes_num+1):
-                if self.nodes[nd_num-1].Y == y:
-                    res.append(nd_num)
-        elif x != 'any' and y == 'any':
-            for nd_num in range(self.nodes_num):
-                if self.nodes[nd_num-1].X == x:
-                    res.append(nd_num)
-        elif x != 'any' and y != 'any':
-            for nd_num in range(self.nodes_num):
-                if self.nodes[nd_num-1].X == x and self.nodes[nd_num-1].Y == y:
-                    res.append(nd_num)
-        return res
-
-    def get_nodes(self, filename):
-        file = open(filename, 'r')
-        line = file.readline()
-        self.nodes = []
-        while True:
-            line = file.readline()
-            if not line:
-                break
-            [X, Y] = list(map(float, line.split()[1:3]))
-            self.nodes.append(Node(X, Y))
-        self.nodes_num = len(self.nodes)
-
-    def get_elements(self, filename):
-        file = open(filename, 'r')
-        line = file.readline()
-        self.elements = []
-        while True:
-            line = file.readline()
-            if not line:
-                break
-            nums = list(map(int, line.split()[2:]))
-            A = [
-                [1, self.nodes[nums[0]-1].X, self.nodes[nums[0]-1].Y],
-                [1, self.nodes[nums[1]-1].X, self.nodes[nums[1]-1].Y],
-                [1, self.nodes[nums[2]-1].X, self.nodes[nums[2]-1].Y]
-            ]
-            self.elements.append(Element(nums, inv(A)))
-        self.elements_num = len(self.elements)
-        
-def fe_solve(mesh, E, nu, sigm0):
-    b = np.array([0.0]*(2*mesh.nodes_num))
-    K = np.array([[0.0]*(2*mesh.nodes_num) for _ in range(2*mesh.nodes_num)])
-
-    def integrate(Ke, be, nums):
-        for i in range(3):
-            for j in range(3):
-                K[nums[i]-1][nums[j]-1] += Ke[2*i][2*j]
-                K[nums[i]][nums[j]] += Ke[2*i+1][2*j+1]
-            b[nums[i]-1] += be[i]
-            b[nums[i]] += be[2*i+1]
-
-    def task(mesh, elem, bond, E, nu):
-        Ke = [[0.0]*6 for _ in range(6)]
-        be = [0.0]*6
-
-        Ni = elem.N[0]
-        Nj = elem.N[1]
-        Nk = elem.N[2]
-
-        BN = np.array([
-            [Ni[1], 0, Nj[1], 0, Nk[1], 0],
-            [0, Ni[2], 0, Nj[2], 0, Nk[2]],
-            [Ni[2], Ni[1], Nj[2], Nj[1], Nk[2], Nk[1]]
-        ])
-
-        invD = np.array([
-            [1, -nu, 0],
-            [-nu, 1, 0],
-            [0, 0, 2*(1+nu)]
-        ])
-
-        D = np.linalg.inv(invD)
-        D = E * D
-
-        xx = [mesh.nodes[elem.nd_nums[0]-1].X, mesh.nodes[elem.nd_nums[1]-1].X, mesh.nodes[elem.nd_nums[2]-1].X]
-        yy = [mesh.nodes[elem.nd_nums[0]-1].Y, mesh.nodes[elem.nd_nums[1]-1].Y, mesh.nodes[elem.nd_nums[2]-1].Y]
-
-        S = triag_square(xx, yy)
-
-        Ke = S * np.matmul(np.matmul(BN.transpose(), D), BN)
-        be = [0]*6
-
-        bnd = []
-        for nd_num in elem.nd_nums:
-            if nd_num in bond:
-                bnd.append(nd_num)
-
-        if len(bnd) == 2:
-            xx = [mesh.nodes[bnd[0]-1].X, mesh.nodes[bnd[1]-1].X]
-            yy = [mesh.nodes[bnd[0]-1].Y, mesh.nodes[bnd[1]-1].Y]
-
-            l = dist(xx, yy)
-
-            x0 = 10.0
-
-            for i in range(3):
-                be[2*i] += elem.N[i][0]*l
-                be[2*i] += elem.N[0][1]*x0*l
-                be[2*i] += elem.N[0][2]*abs(mesh.nodes[bnd[0]-1].Y**2 - mesh.nodes[bnd[1]-1].Y**2)/2
-        
-        return [Ke, be]
-
-    for elem in mesh.elements:
-        [Ke, be] = task(mesh, elem, mesh.nodes_in_loc(10, 'any'), E, nu)
-        integrate(Ke, be, elem.nd_nums)
-
-    rem = np.array([])
-
-    rem = np.append(rem, mesh.nodes_in_loc(0, 'any'))
-    rem = np.append(rem, mesh.nodes_in_loc('any', 0))
-    rem = np.append(rem, rem-1)
-
-    rem = list(map(int, rem))
-
-    K = np.delete(K, rem, 0)
-    b = np.delete(b, rem, 0)
-    K = np.delete(K, rem, 1)
-
-    return [K, b]
-
-# *****************************************************************************************************
-
-
-mesh = Mesh('nodes.txt', 'elements.txt')
-
-E = 2e+11
+E = 2e+6 #Pa
 nu = 0.3
-sigm0 = 1e+7
 
-[K, b] = fe_solve(mesh, E, nu, sigm0)
+K = np.array([[0.0]*(2*node_num) for _ in range(2*node_num)])
+b = np.array([0.0]*(2*node_num))
+
+invD = np.array([
+        [1.0, -nu, 0.0],
+        [-nu, 1.0, 0.0],
+        [0.0, 0.0, 2*(1+nu)]
+    ])
+D = E * np.linalg.inv(invD)
+
+for e in range(elem_num):
+    Ne = N[e]
+    BN = np.array([
+        [Ne[1][0], 0.0, Ne[1][1], 0.0, Ne[1][2], 0.0],
+        [0.0, Ne[2][0], 0.0, Ne[2][1], 0.0, Ne[2][2]],
+        [Ne[2][0], Ne[1][0], Ne[2][1], Ne[1][1], Ne[2][2], Ne[1][2]]
+    ])
+    i = ijk[e][0]
+    j = ijk[e][1]
+    k = ijk[e][2]
+    S = area([Xnode[i], Xnode[j], Xnode[k]], [Ynode[i], Ynode[j], Ynode[k]])
+    Ke = S * np.matmul(np.matmul(BN.transpose(), D), BN)
 
 
-u = np.linalg.solve(K, b)
+
+    x0 = 5.
+    sigm0 = 1e+3
+
+    bond = []
+    for q in ijk[e]:
+        if (Xnode[q] == x0):
+            bond.append(q)
+    be = np.array([0.0]*6)
+    if len(bond) == 2:
+        l = abs(dist([Xnode[bond[0]], Xnode[bond[1]]], [Ynode[bond[0]], Ynode[bond[1]]]))
+        for q in range(3):
+            be[2*q] += Ne[0][q]*l
+            be[2*q] += Ne[1][q]*x0*l
+            be[2*q] += Ne[2][q] * 0.5 * abs(Ynode[bond[0]]**2 - Ynode[bond[1]]**2)
+    sigm0 = 1e+3
+    be = be * sigm0
+
+    for q in range(3):
+        for p in range(3):
+            K[2*ijk[e][q]][2*ijk[e][p]] += Ke[2*q][2*p]
+            K[2*ijk[e][q]+1][2*ijk[e][p]] += Ke[2*q+1][2*p]
+            K[2*ijk[e][q]][2*ijk[e][p]+1] += Ke[2*q][2*p+1]
+            K[2*ijk[e][q]+1][2*ijk[e][p]+1] += Ke[2*q+1][2*p+1]
+
+        b[2*ijk[e][q]] += be[2*q]
+        b[2*ijk[e][q]+1] += be[2*q+1]
+
+
+
+
+nums = range(2*node_num)
+
+rem = []
+for p in range(node_num):
+    if Xnode[p] < 1e-7:
+        rem.append(2*p)
+    if Ynode[p] < 1e-7:
+        rem.append(2*p+1)
+
+
+Krem = np.delete(K, rem, 0)
+Krem = np.delete(Krem, rem, 1)
+
+brem = np.delete(b, rem, 0)
+
+nums = np.delete(nums, rem, 0)
+
+urem = np.linalg.solve(Krem, brem)
+
+#**********************************************************************************************
+
+u = [0.0]*(2*node_num)
+
+i = 0
+for n in range(len(nums)):
+    u[nums[n]] = urem[n]
+
+print(rem)
+print(nums)
+
+#fileX = open(u_x, 'r')
+#fileY = open(u_y, 'r')
+#lineX = fileX.readline()
+#ineY = fileY.readline()
+#u_test = []
+#while True:
+#    lineX = fileX.readline()
+#   lineY = fileY.readline()
+#    if not lineX:
+#        break
+#    u_test.append(float(lineX.split()[-1]))
+#   u_test.append(float(lineY.split()[-1]))
+
+for i in range(node_num):
+    print('%1.2E' % u[2*i])
+    print('%1.2E' % u[2*i+1])
+
+#u = np.linalg.solve(K,b)
+
+print(K.dot(u)-b)
+print(Krem.dot(urem)-brem)
+
+#print(Ke)
---
+++ Kirschv2.py
@@ -0,0 +1,243 @@
+import numpy as np
+
+def area(x, y):
+    A = np.array([
+        [x[0], y[0], 1.0],
+        [x[1], y[1], 1.0],
+        [x[2], y[2], 1.0]
+    ])
+    return 0.5*np.linalg.det(A)
+
+# plate 5mx5m with 1m round hole in x=0 y=0; x=5m has 1e+3 Pa
+
+file_nodes = 'nodes.txt'
+file_elements = 'elements.txt'
+u_x = 'u_x.txt'
+u_y = 'u_y.txt'
+
+file = open(file_nodes, 'r')
+line = file.readline()
+X = []
+Y = []
+ndnum = 0
+while True:
+    line = file.readline()
+    if not line:
+        break
+    [x, y] = list(map(float, line.split()[1:3]))
+    X.append(x)
+    Y.append(y)
+    ndnum += 1
+
+
+
+file = open(file_elements, 'r')
+line = file.readline()
+ijk = []
+N = []
+enum = 0
+while True:
+    line = file.readline()
+    if not line:
+        break
+    l = []
+    for n in list(map(int, line.split()[2:])):
+        l.append(n-1)
+    A = np.array([
+        [1, X[l[0]], Y[l[0]]],
+        [1, X[l[1]], Y[l[1]]],
+        [1, X[l[2]], Y[l[2]]]
+    ])
+    N.append(np.linalg.inv(A).transpose())
+    ijk.append(l)
+    enum += 1
+
+
+
+E = 2e+6
+nu = 0.3
+
+invD = np.array([
+        [1.0, -nu, 0.0],
+        [-nu, 1.0, 0.0],
+        [0.0, 0.0, 2*(1+nu)]
+    ])
+D = E * np.linalg.inv(invD)
+
+K = np.zeros((2*ndnum, 2*ndnum))
+b = np.zeros(2*ndnum)
+
+for e in range(enum):
+    Ne = N[e]
+
+    i = ijk[e][0]
+    j = ijk[e][1]
+    k = ijk[e][2]
+
+    Ni = Ne[0]
+    Nj = Ne[1]
+    Nk = Ne[2]
+    
+    N_i = lambda x,y: Ni[0] + Ni[1]*x + Ni[2]*y
+    N_j = lambda x,y: Nj[0] + Nj[1]*x + Nj[2]*y
+    N_k = lambda x,y: Nk[0] + Nk[1]*x + Nk[2]*y
+
+    BN = np.array([
+        [Ni[1], 0.0, Nj[1], 0.0, Nk[1], 0.0],
+        [0.0, Ni[2], 0.0, Nj[2], 0.0, Nk[2]],
+        [Ni[2], Ni[1], Nj[2], Nj[1], Nk[2], Nk[1]]
+    ])
+
+    S = area([X[i], X[j], X[k]], [Y[i], Y[j], Y[k]])
+    Ke = S * np.matmul(np.matmul(BN.transpose(), D), BN)
+
+
+
+    be = np.zeros(2*ndnum)
+
+    x0 = 5
+    sigm0 = 1e+3
+
+    bnd = []
+    for q in ijk[e]:
+        if (abs(X[q] - x0) < 1e-7):
+            bnd.append(q)
+
+    be = np.zeros(6)
+
+    if len(bnd) == 2:
+        y1 = Y[bnd[0]]
+        y2 = Y[bnd[1]]
+
+        l = abs(y1 - y2)
+        d = abs(y1*y1-y2*y2)/2
+
+        be[0] += Ni[0] * l + Ni[1] * x0 * l + Ni[2] * d
+        be[2] += Nj[0] * l + Nj[1] * x0 * l + Nj[2] * d
+        be[4] += Nk[0] * l + Nk[1] * x0 * l + Nk[2] * d
+
+    be = be*sigm0
+
+    for q in range(3):
+        for p in range(3):
+            K[2*ijk[e][q]][2*ijk[e][p]] += Ke[2*q][2*p]
+            K[2*ijk[e][q]+1][2*ijk[e][p]] += Ke[2*q+1][2*p]
+            K[2*ijk[e][q]][2*ijk[e][p]+1] += Ke[2*q][2*p+1]
+            K[2*ijk[e][q]+1][2*ijk[e][p]+1] += Ke[2*q+1][2*p+1]
+
+        b[2*ijk[e][q]] += be[2*q]
+
+
+
+rem = []
+for p in range(ndnum):
+    if X[p] < 1e-7:
+        rem.append(2*p)
+    if Y[p] < 1e-7:
+        rem.append(2*p+1)
+
+
+Krem = np.delete(K, rem, 0)
+Krem = np.delete(Krem, rem, 1)
+
+brem = np.delete(b, rem, 0)
+
+urem = np.linalg.solve(Krem, brem)
+
+urem = np.linalg.solve(Krem, brem)
+
+nums = np.delete(range(2*ndnum), rem, 0)
+
+
+u = [0.0]*(2*ndnum)
+
+i = 0
+for n in range(len(nums)):
+    u[nums[n]] = urem[n]
+
+
+for z in K.dot(u) - b:
+    if z > 1e+7:
+        print('big diff')
+
+
+
+file = open(u_x, 'r')
+line = file.readline()
+ux = []
+ndnum = 0
+while True:
+    line = file.readline()
+    if not line:
+        break
+    ux.append(float(line.split()[-1]))
+    ndnum += 1
+
+file = open(u_y, 'r')
+line = file.readline()
+uy = []
+ndnum = 0
+while True:
+    line = file.readline()
+    if not line:
+        break
+    uy.append(float(line.split()[-1]))
+    ndnum += 1
+
+for i in range(ndnum):
+    if (abs(u[2*i] - ux[i]) > 1e-3):
+        print('big diff')
+    if (abs(u[2*i+1] - uy[i]) > 1e-3):
+        print('big diff')
+
+ss = []
+xx = []
+for e in range(enum):
+
+    Ne = N[e]
+
+    i = ijk[e][0]
+    j = ijk[e][1]
+    k = ijk[e][2]
+
+    Ni = Ne[0]
+    Nj = Ne[1]
+    Nk = Ne[2]
+
+
+    BN = np.array([
+        [Ni[1], 0.0, Nj[1], 0.0, Nk[1], 0.0],
+        [0.0, Ni[2], 0.0, Nj[2], 0.0, Nk[2]],
+        [Ni[2], Ni[1], Nj[2], Nj[1], Nk[2], Nk[1]]
+    ])
+
+    a = np.array([
+        u[2*i], u[2*i+1], u[2*j], u[2*j+1], u[2*k], u[2*k+1]
+    ])
+
+    eps = BN.dot(a)
+
+    sigm = D.dot(eps)
+
+    if (X[i] < 1e+7):
+        xx.append(Y[i])
+        ss.append(sigm[1])
+    if (X[j] < 1e+7):
+        xx.append(Y[j])
+        ss.append(sigm[1])
+    if (X[k] < 1e+7):
+        xx.append(Y[k])
+        ss.append(sigm[1])
+
+
+import matplotlib.pyplot as plt
+
+
+plt.figure()
+
+plt.scatter(xx, ss)
+print(xx)
+
+plt.show()
+
+
